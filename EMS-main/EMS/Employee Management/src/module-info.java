ed3b547a6c6a2a19a0fe80db27f816874aa51c30
p9131 <101371077+p9131@users.noreply.github.com>
2024-02-18 20:10:54 +0200

    Add files via upload

---
+++ EMS-main/EMS/Employee Management/src/module-info.java
@@ -0,0 +1,8 @@
+module Employee.Management {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.json;
+    requires java.net.http;
+    opens EMS;
+}
